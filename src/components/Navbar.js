90b580d26de72c259e98e3bb8d627f65af5be3c1
Rebotilwe <rebotilwemokiba@gmail.com>
2025-05-27 13:36:05 +0200

    contact

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -37,7 +37,7 @@ const Navbar = () => {
                     <Link to="/about">About</Link>
                 </li>
              <li>
-  <a href="/contact">Contact</a>
+  <Link to ="/contact">Contact</Link>
 </li>
 
             </ul>
